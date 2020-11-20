c6e9012f721bf8867cf2fb612509b912989e7726
zhivar <zhivarsourati@gmail.com>
2020-11-20 12:06:16 +0330

    Complete Clause Coverage for TriType class

--- src/test/java/org/springframework/samples/petclinic/utility/TriTypeTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/TriTypeTest.java
@@ -34,4 +34,44 @@ class TriTypeTest {
 //		predicate = a predicate with any number of clauses
 		return predicate;
 	}
+
+//	CC Tests (Clause Coverage)
+
+	@Test
+	public void recognizeTriangleByCodeTestCC1() {
+		TriType triType = new TriType();
+		TriType.TryClass triClass;
+		triClass = triType.classifyTriangle(1, 1, 1);
+		Assertions.assertEquals(triClass, TriType.TryClass.EQUILATERAL);
+	}
+
+	@Test
+	public void recognizeTriangleByCodeTestCC2() {
+		TriType triType = new TriType();
+		TriType.TryClass triClass;
+		triClass = triType.classifyTriangle(-1, -1, -1);
+		Assertions.assertEquals(triClass, TriType.TryClass.NOT_VALID);
+	}
+
+	@Test
+	public void recognizeTriangleByCodeTestCC3() {
+		TriType triType = new TriType();
+		TriType.TryClass triClass;
+		Assertions.assertEquals(triType.classifyTriangle(1, 2, 3), TriType.TryClass.NOT_VALID);
+		Assertions.assertEquals(triType.classifyTriangle(2, 3, 1), TriType.TryClass.NOT_VALID);
+		Assertions.assertEquals(triType.classifyTriangle(3, 2, 1), TriType.TryClass.NOT_VALID);
+	}
+
+	@Test
+	public void recognizeTriangleByCodeTestCC4() {
+		TriType triType = new TriType();
+		TriType.TryClass triClass;
+		Assertions.assertEquals(triType.classifyTriangle(2, 2, 3), TriType.TryClass.ISO_SCALENE);
+		Assertions.assertEquals(triType.classifyTriangle(3, 2, 2), TriType.TryClass.ISO_SCALENE);
+		Assertions.assertEquals(triType.classifyTriangle(2, 3, 2), TriType.TryClass.ISO_SCALENE);
+		Assertions.assertEquals(triType.classifyTriangle(2, 2, 4), TriType.TryClass.NOT_VALID);
+		Assertions.assertEquals(triType.classifyTriangle(4, 2, 2), TriType.TryClass.NOT_VALID);
+		Assertions.assertEquals(triType.classifyTriangle(2, 4, 2), TriType.TryClass.NOT_VALID);
+	}
+
 }
